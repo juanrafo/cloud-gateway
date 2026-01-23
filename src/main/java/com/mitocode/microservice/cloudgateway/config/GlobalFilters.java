2d70b00101d7b6a9a447c30f27afa2a114256b54
unknown <jrafael.qhilario@gmail.com>
2026-01-23 01:39:25 -0500

    upgrade vesion

--- src/main/java/com/mitocode/microservice/cloudgateway/config/GlobalFilters.java
+++ src/main/java/com/mitocode/microservice/cloudgateway/config/GlobalFilters.java
@@ -4,10 +4,10 @@ import java.util.Optional;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-import java.util.Optional;
 
 @Component
 public class GlobalFilters implements GlobalFilter{
@@ -36,3 +36,5 @@ public class GlobalFilters implements GlobalFilter{
         ));
     }
 }
+
+
